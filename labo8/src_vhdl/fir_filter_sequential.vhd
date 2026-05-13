b5a170585e36f3cd344e9ad40cd1fd946af4dd71
Emily <anny.baquerizolema@heig-vd.ch>
2026-05-13 03:04:26 +0200

    sequential: done

--- labo8/src_vhdl/fir_filter_sequential.vhd
+++ labo8/src_vhdl/fir_filter_sequential.vhd
@@ -32,10 +32,67 @@ entity fir_filter is
 end entity fir_filter;
 
 architecture sequential of fir_filter is 
---- Internal signals and types can be declared here if needed
+    type state_t is (SLEEP, COMPUTE, DONE);
+    signal state_s : state_t;
+
+    -- Signal to hold the intermediate result of the FIR computation, with enough bits to avoid overflow
+    signal result_s : signed(DATASIZE + COEFFSIZE + 7 downto 0);
+    -- Counter to keep track of the number of processed samples
+    signal count_s : natural range 0 to ORDER;
+    
+    type data_array_t is array (0 to ORDER) of signed(DATASIZE - 1 downto 0); -- Type for shift register
+    signal shift_reg_s : data_array_t;
 
 begin
---- FIR filter implementation goes here
+
+    fir_process : process(clk_i, rst_i)
+        variable next_state_s : state_t;
+    begin
+        if rst_i = '1' then
+            state_s <= SLEEP;
+            count_s <= 0;
+            result_s <= (others => '0');
+            shift_reg_s <= (others => (others => '0'));
+        elsif rising_edge(clk_i) then
+            next_state_s := state_s; -- Default to hold state
+            case state_s is
+            when SLEEP =>
+                if din_valid_i = '1' then
+                    shift_reg_s(0) <= signed(din_i);
+
+                    for i in 1 to ORDER loop
+                        shift_reg_s(i) <= shift_reg_s(i - 1);
+                    end loop;
+
+                    next_state_s := COMPUTE;
+                end if;
+
+            when COMPUTE =>
+                if count_s = 0 then
+                    result_s <= resize(shift_reg_s(0) * coeffs_i(0), result_s'length);
+                else
+                    result_s <= result_s + resize(shift_reg_s(count_s) * coeffs_i(count_s), result_s'length);
+                end if;
+                if count_s = ORDER then
+                    next_state_s := DONE;
+                else 
+                    count_s <= count_s + 1;
+                end if;
+            when DONE =>
+                if dout_ready_i = '1' then
+                    next_state_s := SLEEP;
+                    count_s <= 0;
+                end if;
+            end case;
+            state_s <= next_state_s;
+        end if;
+    end process;
+
+    dout_o <= std_logic_vector(result_s(DATASIZE + COMMAPOS - 1 downto COMMAPOS));
+
+    -- Control signals
+    din_ready_o <= '1' when state_s = SLEEP else '0';
+    dout_valid_o <= '1' when state_s = DONE else '0';
 
 
 end architecture sequential;
